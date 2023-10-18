cddee18d1e058dea06fd308ed20dfd5de7a28e1d
Matheus de Almeida <108769812+dalmeidamatheus@users.noreply.github.com>
2023-10-17 22:12:48 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,65 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
-import Button from './components/button';
-import Input from './components/input';
+import { NavigationContainer } from "@react-navigation/native";
+import Comanda from "./screens/Comanda";
+import Login from "./screens/Login";
+import pagi from "./screens/pagi";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Image style={styles.image} source={require('./assets/home.jpg')} />
-      <Text style={styles.title}>comes&bebes</Text>
-
-      <Input placeholder='e-mail'/>
-      <Input placeholder='senha' secureTextEntry/>
-
-      <Button>entrar</Button>
-      
-      <TouchableOpacity>
-        <Text>criar conta</Text>
-      </TouchableOpacity>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown:false}}>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Comanda" component={Comanda} />
+        <Stack.Screen name="pagi" component={pagi} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FCFBFF',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: 900
-  },
-  title: {
-    color: '#d98a6b',
-    fontSize: 34,
-    fontWeight: 'bold',
-    top: 20,
-    marginBottom: 20,
-  },
-  input: {
-    backgroundColor: '#FFF',
-    borderColor: 'gray',
-    borderWidth: 1,
-    padding: 8,
-    borderRadius: 4,
-    marginTop: 40,
-    top: 30,
-  }, 
-  button:{
-    backgroundColor: '#d98a6b',
-    color: '#FFF',
-    paddingVertical: 8,
-    paddingHorizontal: 48,
-    borderRadius: 4,
-    marginTop: 16,
-    marginBottom: 16
-  },
-  image: {
-    height: 340,
-    width: '100%',
-    position: 'absolute',
-    top: 0,
-    marginBottom: 50
-  }
-});
